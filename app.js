aa7975c4f7598d4d1a1b602fd1427888bc631eaa
Stephen Mangai <sm120@students.kiron.ngo>
2023-03-04 07:56:25 +0100

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let userInputs = ["moon", "sun", "day", "dark"];
 let originalStory = `The ${userInputs[0].toUpperCase()} shines only when is ${userInputs[3].toUpperCase()}. The ${userInputs[1].toUpperCase()} shines only when is ${userInputs[2].toUpperCase()}. There is working harmony between the ${userInputs[0].toUpperCase()} and the ${userInputs[1].toUpperCase()}, this is a shift, a permanent shift.`;
+let numberOfQuestions = 4;
 console.log(originalStory);
 
 //let originalStory = "The moon shines only when its dark. The sun shines only when is day. There is working harmony betwwen the sun and the moon, this is a shift, a permanent shift.";
