39c63bce4b3847c539a2f4412d617f6ac52fb8d7
Stephen Mangai <sm120@students.kiron.ngo>
2023-03-04 10:47:51 +0100

    Added a loop

--- app.js
+++ app.js
@@ -3,6 +3,17 @@
 let userInputs = ["moon", "sun", "day", "dark"];
 let originalStory = `The ${userInputs[0].toUpperCase()} shines only when is ${userInputs[3].toUpperCase()}. The ${userInputs[1].toUpperCase()} shines only when is ${userInputs[2].toUpperCase()}. There is working harmony between the ${userInputs[0].toUpperCase()} and the ${userInputs[1].toUpperCase()}, this is a shift, a permanent shift.`;
 let numberOfQuestions = 4;
+
+let planet1 = prompt("Enter a planet: ");
+let planet2 = prompt("Enter a planet: ");
+let day = prompt("Enter the word day: ");
+let dark = prompt("Enter the word dark: ");
+
+let questionArray = [planet1, planet2, day, dark];
+
+for (let i = numberOfQuestions; i > 0; i--){
+    console.log(i);
+}
 console.log(originalStory);
 
 //let originalStory = "The moon shines only when its dark. The sun shines only when is day. There is working harmony betwwen the sun and the moon, this is a shift, a permanent shift.";
